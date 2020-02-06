3ba05f556da638634784b658dbfca5c86a567ed2
20140339 <kva231@naver.com>
2020-02-06 23:07:21 +0900

    N과 M (4)
    백트래킹 입문 문제 4
    
    해결

---
+++ 단계별로 풀어보기/15. 백트랙킹/15652.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			arr[cnt] = i + 1;
+			if (cnt == 0 || arr[cnt] >= arr[cnt - 1]) dfs(cnt + 1);
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	dfs(0);
+}
