72bf8bbaee000e711d7877e1a91e3d51dc51a183
20140339 <kva231@naver.com>
2020-09-03 23:47:03 +0900

    Add java code

---
+++ BOJ_1181/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		String[] arr = new String[n];
+		for (int i = 0; i < n; ++i)
+			arr[i] = br.readLine();
+		br.close();
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.length() == o2.length())
+					return o1.compareTo(o2);
+				else
+					return o1.length() - o2.length();
+			}
+		});
+		
+		String temp = arr[0];
+		bw.write(temp + "\n");
+		for (int i = 1; i < n; ++i) {
+			if (arr[i].equals(temp)) continue;
+			
+			temp = arr[i];
+			bw.write(temp + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
