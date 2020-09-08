1fb47f86e770e3d4365baf5a33d129ce163c82f1
20140339 <kva231@naver.com>
2020-09-08 21:43:04 +0900

    Add java code

---
+++ BOJ_11399/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] p = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; ++i)
+			p[i] = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		Arrays.sort(p);
+		
+		int sum = 0;
+		int temp = 0;
+		for (int i = 0; i < n; ++i) {
+			temp += p[i];
+			sum += temp;
+		}
+		
+		bw.write(String.valueOf(sum));
+		bw.flush();
+		bw.close();
+	}
+}
