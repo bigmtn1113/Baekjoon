ac6b3c11364825eb66521ab3ded5e714fa60a46e
kva231 <46125158+kva231@users.noreply.github.com>
2020-09-16 13:54:27 +0900

    Update Main.java

--- BOJ_1012/Main.java
+++ BOJ_1012/Main.java
@@ -3,98 +3,110 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-	static ArrayList<Integer>[] vertex = new ArrayList[2501];
-	static boolean[] isVisit = new boolean[2501];
-	static int cnt = 0;
+class Pair {
+	int i;
+	int j;
+	
+	Pair(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+	int first() { return i; }
+	int second() { return j; }
+}
 
-	public static void main(String[] args) throws IOException{
+public class Main {
+	static boolean[][] field = new boolean[51][51];
+	static boolean[][] isVisit = new boolean[51][51];
+	static int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};	// 상, 하, 좌, 우
+	static int m, n;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int t = Integer.parseInt(br.readLine());
 		for (int i = 0; i < t; ++i) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int m = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-			int field[][] = new int[n][m];
-			
-			for (int j = 0; j < m * n; ++j)
-				vertex[j] = new ArrayList<>();
 			
 			for (int j = 0; j < k; ++j) {
 				st = new StringTokenizer(br.readLine());
 				int col = Integer.parseInt(st.nextToken());
 				int row = Integer.parseInt(st.nextToken());
-				
-				++field[row][col];
+				field[row][col] = true;
 			}
 			
-			for (int j = 0; j < n; ++j) {
-				for (int l = 0; l < m; ++l) {
-					if (field[j][l] == 0) continue;
-					int curr = j * m + l;
-					
-					if (j > 0 && field[j - 1][l] > 0) vertex[curr].add(curr - m);	// 위
-					if (j < n - 1 && field[j + 1][l] > 0) vertex[curr].add(curr + m);	// 아래
-					if (l > 0 && field[j][l - 1] > 0) vertex[curr].add(curr - 1);	// 왼
-					if (l < m - 1 && field[j][l + 1] > 0) vertex[curr].add(curr + 1);	// 오
-				}
-			}
-			
-			int earthworm = 0;
-			for (int j = 0; j < n; ++j) {
-				for (int l = 0; l < m; ++l) {
-					if (field[j][l] > 0 && !isVisit[j * m + l]) {
-						dfs(j * m + l);
-						//bfs(j * m + l);
+			int earthwormCnt = 0;
+			for (int a = 0; a < n; ++a)
+				for (int b = 0; b < m; ++b)
+					if (field[a][b] && !isVisit[a][b]) {
+						dfs(a, b);
+						//bfs(a, b);
 						
-						++earthworm;
-						cnt = 0;
+						++earthwormCnt;
 					}
-				}
-			}
-			bw.write(earthworm + "\n");
 			
-			for (int j = 0; j < m * n; ++j)
-				isVisit[j] = false;
+			bw.write(earthwormCnt + "\n");
+			
+			for (int a = 0; a < 51; ++a) {
+				Arrays.fill(field[a], false);
+				Arrays.fill(isVisit[a], false);
+			}
 		}
-		
+		br.close();
 		bw.flush();
 		bw.close();
 	}
 
-	static void dfs(int v) {
-		if (isVisit[v]) return;
+	static void dfs(int currI, int currJ) {
+		if (isVisit[currI][currJ]) return;
 		
-		isVisit[v] = true;
-		++cnt;
+		isVisit[currI][currJ] = true;
 		
-		for (int i : vertex[v])
-			if (!isVisit[i])
-				dfs(i);
-	}
-	
-	static void bfs(int v) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(v);
-		isVisit[v] = true;
-		
-		while (!queue.isEmpty()) {
-			int curr = queue.poll();
-			++cnt;
+		for (int k = 0; k < 4; ++k) {
+			int nextI = currI + direction[k][0];
+			int nextJ = currJ + direction[k][1];
 			
-			for (int i : vertex[curr])
-				if (!isVisit[i]) {
-					queue.offer(i);
-					isVisit[i] = true;
-				}
+			if (nextI < 0 || nextI > n - 1 || nextJ < 0 || nextJ > m - 1)
+				continue;
+			
+			if (field[nextI][nextJ] && !isVisit[nextI][nextJ])
+				dfs(nextI, nextJ);
 		}
 	}
+	
+	static void bfs(int currI, int currJ) {
+		Queue<Pair> queue = new LinkedList<>();
+		queue.offer(new Pair(currI, currJ));
+		isVisit[currI][currJ] = true;
+		
+		while (!queue.isEmpty()) {
+			currI = queue.peek().first();
+			currJ = queue.peek().second();
+			queue.poll();
+			
+			for (int k = 0; k < 4; ++k) {
+				int nextI = currI + direction[k][0];
+				int nextJ = currJ + direction[k][1];
+				
+				if (nextI < 0 || nextI > n - 1 || nextJ < 0 || nextJ > m - 1)
+					continue;
+				
+				if (field[nextI][nextJ] && !isVisit[nextI][nextJ]) {
+					queue.offer(new Pair(nextI, nextJ));
+					isVisit[nextI][nextJ] = true;
+				}
+			}
+		}
+	}
+	
 }
