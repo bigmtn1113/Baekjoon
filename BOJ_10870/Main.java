8925553e04263c812aa9099029b66251d68a3802
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-28 21:43:13 +0900

    Create Main.java

---
+++ BOJ_10870/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int fibonachi(int x) {
+		if (x == 0 || x == 1) return x;
+		
+		return fibonachi(x - 1) + fibonachi(x - 2);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+        sc.close();
+		
+		System.out.print(fibonachi(n));
+	}
+
+}
