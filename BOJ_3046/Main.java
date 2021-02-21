facec7bcd358b2768eaaa69d633d5dac38de722b
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-21 15:50:07 +0900

    Create Main.java

---
+++ BOJ_3046/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int r1 = sc.nextInt();
+		int s = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print(s * 2 - r1);
+	}
+	
+}
