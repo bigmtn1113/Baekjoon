e2d7bca0046b5ef0fff0d3c5c8a81acca2409715
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-22 11:45:10 +0900

    Create Main.java

---
+++ BOJ_14645/Main.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		for (int i = 0; i < n; ++i) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		System.out.print("비와이");
+	}
+	
+}
