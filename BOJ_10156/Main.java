bbd411dae6f36bb19dffd9970378260dc94e8214
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-23 19:03:29 +0900

    Create Main.java

---
+++ BOJ_10156/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int k = sc.nextInt();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print((k * n > m) ? (k * n - m) : 0);
+	}
+	
+}
