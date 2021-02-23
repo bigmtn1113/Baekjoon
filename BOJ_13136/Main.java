4c386df22512eb125ae52e284c9f1de1b4d10765
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-23 21:53:24 +0900

    Create Main.java

---
+++ BOJ_13136/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print((long) (Math.ceil((double) r / n) * Math.ceil((double) c / n)));
+	}
+	
+}
