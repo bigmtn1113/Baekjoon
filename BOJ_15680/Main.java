547a274b205f81d984c4af3c08ec461be4d3378a
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-24 01:54:26 +0900

    Create Main.java

---
+++ BOJ_15680/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print((n == 0) ? "YONSEI" : "Leading the Way to the Future");
+	}
+	
+}
