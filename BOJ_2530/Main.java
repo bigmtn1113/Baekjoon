241f7e8c1c1e9d46595c1890b97d66cb6b7b694c
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-23 15:33:38 +0900

    Create Main.java

---
+++ BOJ_2530/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		
+		sc.close();
+		
+		c += d;
+		
+		if (c >= 60) {
+			b += c / 60;
+			c = c % 60;
+		}
+		
+		if (b >= 60) {
+			a += b / 60;
+			b = b % 60;
+		}
+		
+		if (a >= 24) {
+			a %= 24;
+		}
+		
+		System.out.print(a + " " + b + " " + c);
+	}
+	
+}
