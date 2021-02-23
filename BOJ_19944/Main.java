d02a04b5612540e0bd3c70e3ccc2dd5c9c9675e9
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-24 02:58:47 +0900

    Create Main.java

---
+++ BOJ_19944/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		sc.close();
+		
+		String result = "";
+		
+		if (m == 1 || m == 2) {
+			result = "NEWBIE!";
+		} else if (m <= n){
+			result = "OLDBIE!";
+		} else {
+			result = "TLE!";
+		}
+		
+		System.out.print(result);
+	}
+	
+}
