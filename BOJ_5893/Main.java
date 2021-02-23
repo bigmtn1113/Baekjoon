453fc3c0365fc99555e0cbeed69d23e7ff727c85
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-23 18:45:58 +0900

    Create Main.java

---
+++ BOJ_5893/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger n = new BigInteger(sc.next(), 2);
+		
+		sc.close();
+		
+		System.out.print(n.multiply(new BigInteger("17")).toString(2));
+	}
+	
+}
