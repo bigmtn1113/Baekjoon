5e727e6f83012130b4f9234aec9f999bbd2beb0b
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-26 17:09:41 +0900

    Create Main.java

---
+++ BOJ_9550/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while (t > 0) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			
+			int childCnt = 0;
+			
+			for (int i = 0; i < n; ++i) {
+				childCnt += sc.nextInt() / k;
+			}
+			
+			System.out.println(childCnt);
+			
+			--t;
+		}
+		
+		sc.close();
+	}
+	
+}
