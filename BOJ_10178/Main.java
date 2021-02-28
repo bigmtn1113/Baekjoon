ecc79a7fa5c7bee32c3b365d48f370a3d9ff7f3b
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-02-28 16:15:00 +0900

    Create Main.java

---
+++ BOJ_10178/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int i = 0; i < t; ++i) {
+			int c = sc.nextInt();
+			int v = sc.nextInt();
+			
+			System.out.println("You get " + c / v + " piece(s) and your dad gets " + c % v + " piece(s).");
+		}
+		
+		sc.close();
+	}
+	
+}
