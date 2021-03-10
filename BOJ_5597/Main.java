878eca0bb6892670fb2be3db12cad6582541a288
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-03-10 22:18:44 +0900

    Create Main.java

---
+++ BOJ_5597/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		boolean[] input = new boolean[31];
+		
+		for (int i = 0; i < 28; ++i) {
+			input[sc.nextInt()] = true;
+		}
+		
+		sc.close();
+		
+		for (int i = 1; i <= 30; ++i) {
+			if (!input[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+}
