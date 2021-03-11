e11794de193bbc4b07978b9ad2fbbe998aef8a8d
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-03-11 17:56:53 +0900

    Create Main.java

---
+++ BOJ_2745/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		int b = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print(Integer.parseInt(n, b));
+	}
+	
+}
