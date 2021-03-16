210e963b944bcbb01c648c318edcabcad60b58a8
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-03-16 16:38:03 +0900

    Create Main.java

---
+++ BOJ_14581/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+		
+		sc.close();
+		
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				if ((i == 1) && (j == 1)) {
+					System.out.print(":" + id + ":");
+				} else {
+					System.out.print(":fan:");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+	
+}
