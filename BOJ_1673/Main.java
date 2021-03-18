6d1cee11893b5d1238c98f003e66ec8fb50e453e
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-03-18 17:56:05 +0900

    Create Main.java

---
+++ BOJ_1673/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextInt()) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			
+			int cnt = n;
+			
+			while (n >= k) {
+				cnt += n / k;
+				n = n / k + n % k;
+			}
+			
+			System.out.println(cnt);
+		}
+		
+		sc.close();
+	}
+	
+}
