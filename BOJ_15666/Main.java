3cf5ba07ca808540fd1884e3330c1ffbf25c21f7
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-04-22 02:20:17 +0900

    Create Main.java

---
+++ BOJ_15666/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Main {
+	public static LinkedHashSet<String> set = new LinkedHashSet<>();
+	
+	public static void recursionFunc(int[] array, int m, int preIndex, String result, int cnt) {
+		if (cnt == m) {
+			set.add(result);
+			return;
+		}
+		
+		for (int i = 0; i < array.length; ++i) {
+			if (i >= preIndex) {
+				recursionFunc(array, m, i, result + array[i] + " ", cnt + 1);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] array = new int[n];
+		
+		for (int i = 0; i < n; ++i) {
+			array[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		Arrays.sort(array);
+		
+		recursionFunc(array, m, -1, "", 0);
+		
+		StringBuilder answer = new StringBuilder();
+		
+		for (String result : set) {
+			answer.append(result).append("\n");
+		}
+		
+		System.out.print(answer);
+	}
+
+}
