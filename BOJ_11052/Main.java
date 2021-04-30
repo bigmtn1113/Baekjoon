0f2a91706ade704a2e441483fe3aca6c82568967
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-04-30 19:34:20 +0900

    Create Main.java

---
+++ BOJ_11052/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] p = new int[n + 1];
+		
+		for (int i = 1; i <= n; ++i) {
+			p[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int[] dp = new int[n + 1];
+		
+		for (int i = 1; i <= n; ++i) {
+			dp[i] = p[i];
+			
+			for (int j = 1; j < i; ++j) {
+				dp[i] = Math.max(dp[i], dp[j] * (i / j) + dp[i % j]);
+			}
+		}
+		
+		System.out.print(dp[n]);
+	}
+
+}
