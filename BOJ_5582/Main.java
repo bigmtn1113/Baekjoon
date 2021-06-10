6496710f57de634dac73bfb193612c0787c5f1f4
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-06-10 16:09:43 +0900

    Create Main.java

---
+++ BOJ_5582/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char[] s1 = sc.next().toCharArray();
+        char[] s2 = sc.next().toCharArray();
+        
+        sc.close();
+        
+        int[][] dp = new int[4001][4001];
+        
+        int answer = 0;
+        
+        for (int i = 1; i <= s1.length; ++i) {
+            for (int j = 1; j <= s2.length; ++j) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    answer = Math.max(answer, dp[i][j]);
+                }
+            }
+        }
+        
+        System.out.print(answer);
+    }
+
+}
